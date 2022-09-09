35c27a7dcbad20a84261709c5f68aa9003d95fb5
Rawan Abdo <abdo_rawan@hotmail.com>
2022-09-10 00:15:01 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express') // require statement has imported the express function from the express package (which was installed 'npm install express')
+const app = express()
+
+app.listen(5000, () => {
+    
+})
