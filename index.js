26a25124a24600cbcc258df2c3332d04dff9ec04
Rawan Abdo <abdo_rawan@hotmail.com>
2022-09-11 23:39:55 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express') // require statement has imported the express function from the express package (which was installed 'npm install express')
+// install express.js package into this proj directory 
+// cmd > 'npm install express'
+
+// require statement has imported the express function from the express package
+const express = require('express')
+
+// initialise an express app by calling this express function
 const app = express()
 
 app.listen(5000, () => {
-    
+    console.log('Server started!')
+    console.log('Listening on port 5000')
 })
