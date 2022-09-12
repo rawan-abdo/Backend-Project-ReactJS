b8e7c0a3219d3f1d12c728e913739358346f39b2
Rawan Abdo <abdo_rawan@hotmail.com>
2022-09-12 23:36:42 +1000

    Update index.js

--- index.js
+++ index.js
@@ -18,13 +18,24 @@ app.use(express.json())
 
 // populate a database to create possible responses from server
 let database = {johnscafe: 'Rated 10/10'}
-// get route
-router.get('/venues', (req, res) => {})
+
+// get route (ARC by Mulesoft can be used to test requests being sent to routes)
+router.get('/venues', (req, res) => {
+    res.send(database[req.query.name] || 'Venue not found!')
+})
+
 
 // listen to events on port 5000 and write some feedback messages
+// to see the log messages excecute >npm run start
 app.listen(5000, () => {
     console.log('Server started!')
     console.log('Listening on port 5000')
 })
 
-// to see the log messages excecute >npm run start
+// http://localhost:5000/venues?name=johnscafe returns 'Rated 10/10'
+
+// POST route : send data to the backend and manipulate the database
+app.post('/venues', (req, res) => {
+    Object.assign(database, req.body)
+    console.log('New data added to database')
+})
